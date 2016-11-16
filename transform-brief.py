3c8a29a413f975ffdaaa8f6117a283923810fabf
Landon Bouma <landon.bouma@gmail.com>
2016-11-16 00:42:21 -0600

    More working on new transform script.

--- transform-brief.py
+++ transform-brief.py
@@ -55,16 +55,12 @@ class TxTl_Argparser(argparse_wrap.ArgumentParser_Wrap):
 			type=str,
 		)
 
-		self.add_argument('-u', '--user', dest='user',
+		self.add_argument('-u', '--user', dest='username',
 			type=str, help="username",
 		)
 
-		self.add_argument('-p', '--pass', dest='pass',
+		self.add_argument('-p', '--pass', dest='password',
 			type=str, help="password",
-		)
-
-		self.add_argument('-m', '--maps', dest='maps_file',
-			type=str, help="py file containing ticket IDs lookup",
 		)
 
 	def verify(self):
@@ -137,9 +133,6 @@ class Transformer(argparse_wrap.Simple_Script_Base):
 		self.entries.append(new_entry)
 
 	def upload_to_tempo(self):
-		__import__(self.cli_opts.maps_file)
-
-		proj_id_parser = re.compile(r'.* \[(\d+):(\d+):([-a-zA-Z0-9]+)\]\w*')
 
 		self.entries = []
 		with open(self.cli_opts.briefs_file, 'r') as briefs_f:
@@ -156,44 +149,48 @@ class Transformer(argparse_wrap.Simple_Script_Base):
 #			self.update_entries(forreal)
 		else:
 			print("ERROR: Found %d error(s)." % (len(self.errs),))
-			print(self.errs)
+			for err in self.errs:
+				print(err)
 			sys.exit(1)
 
 	def update_entries(self, forreal):
 		#print(self.entries)
 		#print(json.dumps(self.entries, sort_keys=True, indent=4))
 
+		proj_id_parser = re.compile(r'.* \[(\d+):(\d+):([-a-zA-Z0-9]+)\]\w*')
+
 		for entry in self.entries:
 
 			# The project ID and item key are encoded in the Activity name.
-			mat = proj_id_parser.match(entry.activity_name)
+			mat = proj_id_parser.match(entry['activity_name'])
 			tup = mat.groups() if mat else None
 			if not tup:
-				self.errs.append("ERROR: Activity name missing JIRA IDs: %s" % (entry.activity_name,))
+				self.errs.append("ERROR: Activity name missing JIRA IDs: %s" % (entry['activity_name'],))
 				continue
 			try:
 				proj_id, item_id, item_key = tup
 			except ValueError as err:
 				self.errs.append(
 					"ERROR: Failed to parse JIRA IDs [proj:item:key] in activity name: %s"
-					% (entry.activity_name,)
+					% (entry['activity_name'],)
 				)
 				continue
 
 			# For client-level Activities (sans ticket numbers),
 			# a tag should contain the item_key.
-			tags = ','.split(entry.tags)
-			for tag in tags:
-				try:
-					prefix, item_key, item_id = '+'.split(tag)
-				except ValueError as err:
-					# Not an encoded tag; ignore.
-					pass
+			if entry['tags']:
+				tags = ','.split(entry['tags'])
+				for tag in tags:
+					try:
+						prefix, item_key, item_id = '__'.split(tag)
+					except ValueError as err:
+						# Not an encoded tag; ignore.
+						pass
 
 			curr_entry = {
-				"dateStarted": "%sT00:00:00.000+0000" % (entry.year_month_day,),
-				"timeSpentSeconds": "%d" % (int(60 * 60 * entry.time_spent),),
-				"comment": "\\n\\n".join(entry.desctimes),
+				"dateStarted": "%sT00:00:00.000+0000" % (entry['year_month_day'],),
+				"timeSpentSeconds": "%d" % (int(60 * 60 * entry['time_spent']),),
+				"comment": "\\n\\n".join(entry['desctimes']),
 				"issue": {
 					"projectId": proj_id,
 					"key": item_key,
@@ -201,26 +198,27 @@ class Transformer(argparse_wrap.Simple_Script_Base):
 					#item_id
 				},
 				"author": {
-					"name": self.cli_opts.user,
+					"name": self.cli_opts.username,
 				},
 			}
 
-			print(curr_entry)
-			sys.exit(0)
 
 			headers = {'content-type': 'application/json',}
 			if not forreal:
 				json.dumps(curr_entry)
 			else:
+				print(curr_entry)
+#
+				continue
 				req = requests.post(
 					self.cli_opts.tempo_url + '/rest/tempo-timesheets/3/worklogs',
-					auth=(self.cli_opts.user, self.cli_opts.pass)),
+					auth=(self.cli_opts.username, self.cli_opts.password),
 					data=json.dumps(curr_entry),
 					headers=headers,
 				)
 
 # FIXME: TRY Just 1 for now.
-			sys.exit(0)
+#			sys.exit(0)
 
 
 if (__name__ == '__main__'):
